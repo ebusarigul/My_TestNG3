359531daeb86927d5e185508e6a67579c4ff8031
Ebubekir Sarigul <esargl147@gmail.com>
2022-08-12 21:37:48 +0300

    proje5 xml hatası çözülemedi

---
+++ src/Proje5/Proje5_Elements.java
@@ -0,0 +1,66 @@
+package Proje5;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class Proje5_Elements {
+
+    public Proje5_Elements(WebDriver driver) {
+        PageFactory.initElements(driver,this);
+    }
+
+    @FindBy(linkText = "Log in")
+    public WebElement logginButton;
+
+    @FindBy(id = "Email")
+    public WebElement email;
+
+    @FindBy(id = "Password")
+    public WebElement password;
+
+    @FindBy(css = "[value=\"Log in\"]")
+    public WebElement loginBtn;
+
+    @FindBy(linkText = "Log out")
+    public WebElement logOut;
+
+    @FindBy(css = "[class=\"validation-summary-errors\"]>ul>li")
+    public WebElement invalidMessage;
+
+    @FindBy(xpath = "(//*[@value=\"Add to cart\"])[2]")
+    public WebElement laptopAddCart;
+
+    @FindBy(id = "topcartlink")
+    public WebElement shoppingCard;
+
+    @FindBy(id = "termsofservice")
+    public WebElement iAgree;
+
+    @FindBy(id = "checkout")
+    public WebElement checOut;
+
+    @FindBy(css = "[id=\"billing-buttons-container\"]>input")
+    public WebElement contunie1;
+
+    @FindBy(css = "[id=\"shipping-buttons-container\"]>input")
+    public WebElement contunie2;
+
+    @FindBy(css = "[id=\"shipping-method-buttons-container\"]>input")
+    public WebElement contunie3;
+
+    @FindBy(css = "[id=\"payment-method-buttons-container\"]>input")
+    public WebElement contunie4;
+
+    @FindBy(css = "[id=\"payment-info-buttons-container\"]>input")
+    public WebElement contunie5;
+
+    @FindBy(css = "[id=\"confirm-order-buttons-container\"]>input")
+    public WebElement confirm;
+
+    @FindBy(css = "[class=\"section order-completed\"] strong")
+    public WebElement result;
+
+
+}
---
+++ src/Proje5/Proje5_POM.java
@@ -0,0 +1,71 @@
+package Proje5;
+
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.testng.Assert;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+public class Proje5_POM extends ProjeDriver {
+
+    @Test(groups = "valideLogin")
+    void senaryo1(){
+
+        Proje5_Elements elements = new Proje5_Elements(driver);
+        elements.logginButton.click();
+        elements.email.sendKeys("esargl147@gmail.com");
+        elements.password.sendKeys("123456");
+
+        wait.until(ExpectedConditions.elementToBeClickable(elements.loginBtn)).click();
+
+        Assert.assertTrue(elements.logOut.isDisplayed());
+
+        elements.logOut.click();
+    }
+
+    @Test(groups = "invalidLogin")
+    void senaryo2(){
+
+        Proje5_Elements elements1 = new Proje5_Elements(driver);
+
+        elements1.logginButton.click();
+        elements1.email.sendKeys("esargl147@gmail.com");
+        elements1.password.sendKeys("1234567");
+
+        wait.until(ExpectedConditions.elementToBeClickable(elements1.loginBtn)).click();
+        Assert.assertTrue(elements1.invalidMessage.getText().contains("incorrect"));
+
+        elements1.email.clear();
+        elements1.email.sendKeys("esargl@gmail.com");
+        elements1.password.sendKeys("123456");
+
+        wait.until(ExpectedConditions.elementToBeClickable(elements1.loginBtn)).click();
+        Assert.assertTrue(elements1.invalidMessage.getText().contains("No customer"));
+
+        elements1.email.clear();
+
+    }
+
+    @Test(groups = "shopping")
+    void senaryo3(){
+
+        Proje5_Elements elements2 = new Proje5_Elements(driver);
+
+        ProjeDriver.loginOl();
+        wait.until(ExpectedConditions.elementToBeClickable(elements2.laptopAddCart)).click();
+        wait.until(ExpectedConditions.elementToBeClickable(elements2.shoppingCard)).click();
+        wait.until(ExpectedConditions.elementToBeClickable(elements2.iAgree)).click();
+        wait.until(ExpectedConditions.elementToBeClickable(elements2.checOut)).click();
+        wait.until(ExpectedConditions.elementToBeClickable(elements2.contunie1)).click();
+        wait.until(ExpectedConditions.elementToBeClickable(elements2.contunie2)).click();
+        wait.until(ExpectedConditions.elementToBeClickable(elements2.contunie3)).click();
+        wait.until(ExpectedConditions.elementToBeClickable(elements2.contunie4)).click();
+        wait.until(ExpectedConditions.elementToBeClickable(elements2.contunie5)).click();
+        wait.until(ExpectedConditions.elementToBeClickable(elements2.confirm)).click();
+
+        Assert.assertTrue(elements2.result.getText().contains("successfully processed!"));
+
+    }
+
+
+
+}
